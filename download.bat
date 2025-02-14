3812d10e4cab9bc04ee3c09f93d3c0617ff8dd01
Igor <50088049+MochyPei@users.noreply.github.com>
2025-02-14 23:58:04 +0300

    Update download.bat
    A temporary fix has been added to prevent accidental downloading of the entire playlist.

--- download.bat
+++ download.bat
@@ -8,7 +8,7 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`powershell -sta "add-type -as System.Windows
 echo %url% | findstr /C:"https://" >nul
 if %errorlevel% equ 0 (
 	echo Downloading...
-	yt-dlp -o "{%%(uploader)s} %%(fulltitle)s.%%(ext)s" -x --audio-quality 0 --audio-format mp3 "%url%"
+	yt-dlp --no-playlist -o "{%%(uploader)s} %%(fulltitle)s.%%(ext)s" -x --audio-quality 0 --audio-format mp3 "%url%"
 	)
 ) else (
 	echo Clipboard does not contain url
