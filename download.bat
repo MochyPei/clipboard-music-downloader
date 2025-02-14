adc313f053eb0cc1c8a0eadb646a122bfeac6425
Igor <kotyara1554@gmail.com>
2025-02-14 18:27:12 +0300

    Create download.bat
    Script that i wrote with help of GPT4, uses powershell to get clipboard, and after, starts download from url using yt-dlp and ffmpeg (to format to mp3).

---
+++ download.bat
@@ -0,0 +1,18 @@
+@echo off
+chcp 1251 >nul
+setlocal
+FOR /F "tokens=* USEBACKQ" %%F IN (`powershell -sta "add-type -as System.Windows.Forms; [windows.forms.clipboard]::GetText()"`) DO (
+    SET url=%%F
+)
+
+echo %url% | findstr /C:"https://" >nul
+if %errorlevel% equ 0 (
+	echo Downloading...
+	yt-dlp -o "{%%(uploader)s} %%(fulltitle)s.%%(ext)s" -x --audio-quality 0 --audio-format mp3 "%url%"
+	)
+) else (
+	echo Clipboard does not contain url
+	echo %url%
+)
+timeout /t 5 /nobreak >nul
+endlocal
